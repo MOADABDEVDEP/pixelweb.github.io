3f1413eda9c0271173079cf76efbb13195fa763f
Matin Sanei <saneimatin33@gmail.com>
2026-02-16 22:16:55 +0330

    Fix asset paths for GitHub Pages project deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/pixelweb.github.io',
+  assetPrefix: '/pixelweb.github.io',
   images: {
     unoptimized: true,
   },
